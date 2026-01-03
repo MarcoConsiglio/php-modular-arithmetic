322caf675291ac92da0b4bd34dbc903800924496
Marco Consiglio <mrccnsgl@gmail.com>
2026-01-03 14:32:01 +0000

    test: Integer modular sum operation

---
+++ tests/Unit/IntegerModularSumTest.php
@@ -0,0 +1,71 @@
+<?php
+namespace Marcoconsiglio\ModularArithmetic\Tests\Unit;
+
+use PHPUnit\Framework\Attributes\TestDox;
+use Marcoconsiglio\ModularArithmetic\IntegerModularSum;
+use Marcoconsiglio\ModularArithmetic\ModularInteger;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[TestDox("The IntegerModularSum")]
+#[CoversClass(IntegerModularSum::class)]
+#[UsesClass(ModularInteger::class)]
+class IntegerModularSumTest extends TestCase
+{
+    #[TestDox("sums two ModularInteger instances.")]
+    public function test_result_is_modular_integer(): void
+    {
+        // Arrange
+        $a = $this->randomModularInteger(max: self::MAX_INTEGER);
+        $b = new ModularInteger(
+            $this->randomInteger(max: self::MAX_INTEGER), 
+            $a->modulus
+        );
+
+        // Act & Assert
+        $this->assertInstanceOf(
+            ModularInteger::class, 
+            (new IntegerModularSum($a, $b))->result()
+        );
+    }
+
+    #[TestDox("result is a value that if it is a multiple of its modulus equals zero.")]
+    public function test_result_is_modular(): void
+    {
+        // Arrange
+        $modulus = 12;
+        $a = new ModularInteger($modulus, $modulus);
+        $b = new ModularInteger($modulus * 2, $modulus);
+        $sum = new IntegerModularSum($a, $b);
+
+        // Act & Assert
+        $this->assertEquals(0, $sum->result()->value);
+    }
+
+    #[TestDox("invariance property states that by increasing or decreasing two 
+    congruent numbers modulo n by the same amount, the new numbers obtained 
+    are still congruent to each other modulo n.")]
+    public function test_invariance_property(): void
+    {
+        // Arrange
+        $a = $this->randomModularInteger(max: self::MAX_INTEGER);
+        $b = new ModularInteger(
+            $this->getCongruentIntegerValue($a->value, $a->modulus, 1),
+            $a->modulus
+        );
+        $c = new ModularInteger(
+            $this->randomInteger(max: self::MAX_INTEGER, sign: 1),
+            $a->modulus
+        );
+
+        // Act
+        $a_plus_c = (new IntegerModularSum($a, $c))->result();
+        $b_plus_c = (new IntegerModularSum($b, $c))->result();
+        
+        // Assert
+        $this->assertTrue(
+            $a_plus_c->isCongruent($b_plus_c), 
+            $this->congruentFailure($a_plus_c, $b_plus_c)
+        );
+    }
+}
